d4fbd2f6da70c8ea1eb25146fcd9c2badfb72437
ilhamgusti <ilham.wibowo51@gmail.com>
2020-07-12 17:58:15 +0700

    chatMessage

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,77 @@
 // initial app files
+
+const io = require('socket.io-client');
+const randomNameGenerator = require('random-animal-name-generator');
+
+const socket = io('localhost:5001');
+
+const msgerForm = get('.msger-inputarea');
+const msgerChat = get('.msger-chat');
+const msgInput = get('.msger-input');
+
+const username = sessionStorage.getItem('username') || randomNameGenerator();
+
+function get(selector, root = document) {
+    return root.querySelector(selector);
+}
+
+socket.on('connect', () => {
+    console.log('Connected to server');
+})
+
+socket.on('disconnect', () => {
+    console.log('Terputus dari server');
+})
+
+// socket.emit('chatMessage', {
+//     user: 'abdul',
+//     message: 'yokk semangat yok'
+// })
+
+
+msgerForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const msgText = msgInput.value;
+    if (!msgText) return;
+    if (msgText === ' ') return;
+
+    appendMessage(username, msgText, 'right', function () {
+        socket.emit('chatMessage', {
+            user: username,
+            message: msgText
+        })
+    });
+    msgInput.value = "";
+
+})
+
+function appendMessage(username, msgText, position = 'right', callback) {
+    const msgHTML = `
+    <div class="msg ${position}-msg">
+                <div class="msg-img" style="background-image: url(https://image.flaticon.com/icons/svg/145/145867.svg)">
+                </div>
+
+                <div class="msg-bubble">
+                    <div class="msg-info">
+                        <div class="msg-info-name">${username}</div>
+                        <div class="msg-info-time">${Date.now()}</div>
+                    </div>
+
+                    <div class="msg-text">
+                        ${msgText}
+                    </div>
+                </div>
+            </div>
+            `;
+    msgerChat.insertAdjacentHTML('beforeend', msgHTML);
+    msgerChat.scrollTop += 500;
+
+    if (callback) callback();
+}
+
+
+socket.on('message', data => {
+    console.log(data);
+    appendMessage(data.user, data.message, 'left')
+})
--- server.js
+++ server.js
@@ -1,1 +1,36 @@
-//initial files
+const express = require('express');
+const Bundler = require('parcel-bundler');
+const http = require('http');
+const PORT = 5001;
+let app = express();
+
+let bundler = new Bundler('./frontend/chat.html')
+app.use(bundler.middleware())
+
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+
+server.listen(PORT, () => {
+    console.log('server running on port: ' + PORT)
+});
+
+
+io.on('connect', socket =>{
+    console.log('User connect ke server');
+
+    socket.on('disconnect', () => {
+        console.log('User Terputus dari server');
+    })
+
+    socket.on('chatMessage', data => {
+        socket.broadcast.emit('message', data);
+        console.log(data);
+    })
+})
+
+
+
+
+
+
